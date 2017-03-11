ab72ce07bb2d95d00ffd22846b442ad22fe720a6
ey1031 <eyudeveloper@gmail.com>
2017-03-10 22:21:48 -0800

    init new website

---
+++ js/header.js
@@ -0,0 +1,8 @@
+// Changes the header highlight while scrolling
+(function($) {
+    "use strict";
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+})(jQuery);
